9a30f5260c3498230523d7799229aeea0a818d41
maliqtaylor <maliq.taylor@icloud.com>
2023-12-04 16:48:34 -0500

    add external links

--- src/components/TopicsShow.js
+++ src/components/TopicsShow.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 
 import "./TopicsShow.css";
 
@@ -12,8 +12,8 @@ function TopicsShow() {
   let { id, topicId } = useParams();
   const samples = content[id][topicId] || [];
 
-  const topic = topics[id].find(t => t.id === Number(topicId));
-  const links = samples.links || [];
+  const topic = topics[id].find((t) => t.id === Number(topicId));
+  const links = topic.links || [];
   const exercises = topic.exercises || [];
 
   return (
@@ -30,26 +30,32 @@ function TopicsShow() {
         </div>
       ))}
       <div className="links-container">
-        {
-          links.length === 0 ? (
-            <p className="no-links">No links for this topic</p>
-          ) : (
-            <ul>
-              {links.map((link, index) => (
-                <li key={index}>
-                  <a href={link.url} target="_blank" rel="noopener noreferrer">{link.title}</a>
-                </li>
-              ))}
-            </ul>
-          )
-        }
+        <h1>External Links</h1>
+        {links.length === 0 ? (
+          <p className="no-links">No links for this topic</p>
+        ) : (
+          <ul>
+            {links.map((link, index) => (
+              <li key={index}>
+                <a href={link.url} target="_blank" rel="noopener noreferrer">
+                  {link.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
       <div className="exercises-container">
         <h2 className="exercises-heading">Exercises</h2>
         <div className="exercise-card">
           {exercises.map((repo, index) => (
             <div key={index} className="exercise-item">
-              <a href={repo.url} target="_blank" rel="noopener noreferrer" className="exercise-link">
+              <a
+                href={repo.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="exercise-link"
+              >
                 {repo}
               </a>
               {/* Include additional repo details if needed */}
